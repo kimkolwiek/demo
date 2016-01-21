705ad96b4baf52ae7d4cc32592d95f61622e4957
kotek <kmazur@student.agh.edu.pl>
2016-01-21 03:17:43 +0100

    commit

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,12 +11,14 @@ namespace ConsoleApplication2
         private static int cos;
         private static int cos1;
         private static int cos2;
+        private static int cos3;
 
         static void Main(string[] args)
         {
             cos = 1;
             cos1 = 2;
             cos2 = 3;
+            cos3 = 4;
         }
     }
 }
